9068a66c128c301331e8c47ec53d2524ceeb030b
Eternity-Sky <tengyumo666@hotmail.com>
2024-11-23 14:19:12 +0800

    css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { Layout, Menu, Typography } from 'antd';
 import ProblemList from './pages/ProblemList';
 import ProblemDetail from './pages/ProblemDetail';
 import 'antd/dist/antd.css';
+import './styles/global.css';
 
 const { Header, Content } = Layout;
 const { Title } = Typography;
@@ -16,21 +17,21 @@ const App: React.FC = () => {
   return (
     <Layout className="layout">
       <Header style={{ display: 'flex', alignItems: 'center' }}>
-        <Title level={3} style={{ margin: '0 24px 0 0', color: 'white' }}>
+        <Title level={3} className="header-title">
           OI 在线评测系统
         </Title>
         <Menu 
           theme="dark" 
           mode="horizontal" 
           defaultSelectedKeys={['1']}
-          style={{ flex: 1 }}
+          className="main-menu"
         >
           <Menu.Item key="1">
             <Link to="/">题目列表</Link>
           </Menu.Item>
         </Menu>
       </Header>
-      <Content style={{ padding: '24px', minHeight: 'calc(100vh - 64px)', background: '#fff' }}>
+      <Content className="content-wrapper">
         <Routes>
           <Route path="/" element={<ProblemList />} />
           <Route path="/problem/:id" element={<ProblemDetail />} />
